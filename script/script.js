bcb2e9338a1266e2aac2b30aa4e6e1add086406e
Norman Higgs <normcrazy@yahoo.com>
2019-01-14 18:58:28 -0600

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+/*SlideShow images*/
 function currentDiv(n) {
   showDivs(slideIndex = n);
 }
@@ -21,3 +22,27 @@ function showDivs(n) {
   x[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " w3-opacity-off";
 }
+
+/* Prices and add to cart*/
+$(document).ready(function () {
+  var ffPrice = 9.99
+  var mgPrice = 19.99
+  var mgs4Price = 29.99
+  var haloPrice = 19.99
+  var gowPrice = 19.99
+  var drPrice = 29.99
+  var counter = 0;
+
+  $("#ffPrice").html(ffPrice);
+  $("#mgPrice").html(mgPrice);
+  $("#mgs4Price").html(mgs4Price);
+  $("#haloPrice").html(haloPrice);
+  $("#gowPrice").html(gowPrice);
+  $("#drPrice").html(drPrice);
+
+  $("#ff8Btn, #mgsBtn, #mgs4Btn, #haloBtn, #gowBtn, #drBtn").click(function(){
+    counter++;
+    $("#cartNum").text(counter);
+  });
+
+});
