1f68354d4d6eefd452b4d2e93dc9789a4aa71a7c
Norman Higgs <normcrazy@yahoo.com>
2019-01-16 19:31:13 -0600

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,36 +1,11 @@
-/*SlideShow images*/
-function currentDiv(n) {
-  showDivs(slideIndex = n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  if (n > x.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = x.length
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-opacity-off", "");
-  }
-  x[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " w3-opacity-off";
-}
-
 /* Prices and add to cart*/
 $(document).ready(function () {
-  var ffPrice = 9.99
-  var mgPrice = 19.99
-  var mgs4Price = 29.99
-  var haloPrice = 19.99
-  var gowPrice = 19.99
-  var drPrice = 29.99
+  var ffPrice = "$9.99";
+  var mgPrice = "19.99";
+  var mgs4Price = "29.99";
+  var haloPrice = "19.99";
+  var gowPrice = "19.99";
+  var drPrice = "29.99";
   var counter = 0;
 
   $("#ffPrice").html(ffPrice);
